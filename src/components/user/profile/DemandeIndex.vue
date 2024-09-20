8b15bb2c485b77030a455c38b1a999995334ff95
r00tMG <meissagningue7@gmail.com>
2024-09-20 16:20:31 +0100

    test sur Demande.vue console.log(demandes)

--- src/components/user/profile/DemandeIndex.vue
+++ src/components/user/profile/DemandeIndex.vue
@@ -1,5 +1,17 @@
 <script setup>
+import {onMounted, ref} from "vue";
+import axios from "@/axios.js";
 
+const demandes = ref([])
+onMounted(async ()=>{
+  const r = axios.get('/demnades',{
+    headers: {
+      'Authorization':`Bearer ${localStorage.getItem('token')}`
+    }
+  })
+  demandes.value = await r.data
+  console.log(demandes.value)
+})
 </script>
 
 <template>
