ac7b554d77e61ae6453320e2ab570c3800657762
r00tMG <meissagningue7@gmail.com>
2024-09-21 14:35:46 +0100

    test TableOrder.vue

--- src/components/admin/TableOrder.vue
+++ src/components/admin/TableOrder.vue
@@ -1,5 +1,16 @@
 <script setup>
+import {onMounted} from "vue";
+import axios from "@/axios.js";
 
+onMounted(async ()=>{
+  const r = await axios.get('index/orders',{
+    headers:{
+      'Authorization':`Bearer ${localStorage.getItem('token')}`
+    }
+  })
+  const orders = await r.data.orders
+  console.log(orders)
+})
 </script>
 
 <template>
