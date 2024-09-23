51fa47a899d0c266f93ea010f0891dee96d4ad48
r00tMG <meissagningue7@gmail.com>
2024-09-23 12:33:15 +0100

    update configuration vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+const env = loadEnv(mode, process.cwd(), '')
+
 export default defineConfig({
   plugins: [
     vue(),
