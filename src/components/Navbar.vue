70493bae77b673dc2ed365c4c64c6f9f69cf6a9c
r00tMG <meissagningue7@gmail.com>
2024-09-23 13:15:34 +0100

    update configuration vite.config.js

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,7 +29,7 @@ export default {
               <router-link to="/"  class="nav-link text-dark" :class="{ active: $route.path === `/` }">Home</router-link>
             </li>-->
           </ul>
-            <li v-if="token">
+            <li class="ms-2" v-if="token">
               <router-link to="/messagerie/index">
                 <span class="icon-count">29</span>
                 <i class="fa fa-envelope fa-2x text-success"></i>
