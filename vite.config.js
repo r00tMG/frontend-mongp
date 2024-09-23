1fc2e5354b5ca7d7759f96446abe4cd35a83b597
r00tMG <meissagningue7@gmail.com>
2024-09-23 13:21:49 +0100

    update configuration vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import {defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Charge les variables d'environnement
+  const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
     define: {
-      __VITE_PUSHER_APP_KEY__: JSON.stringify(import.meta.env.VITE_PUSHER_APP_KEY),
-      __VITE_PUSHER_APP_CLUSTER__: JSON.stringify(import.meta.env.VITE_PUSHER_APP_CLUSTER)
-    }
+      __VITE_PUSHER_APP_KEY__: JSON.stringify(env.VITE_PUSHER_APP_KEY),
+      __VITE_PUSHER_APP_CLUSTER__: JSON.stringify(env.VITE_PUSHER_APP_CLUSTER),
+    },
   }
-})
+});
